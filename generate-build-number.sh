963357d06650be68111d2cbfdcb6f01b7e79ba07
xlfdll <xlfdll@gmail.com>
2020-03-31 13:41:59 -0700

    Updated build number generation script

--- generate-build-number.sh
+++ generate-build-number.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#version=`cat version.txt`
+# version=`cat version.txt`
 build=$[`git log --oneline | wc -l`]
 builddate=`date +"%F" `
 buildtime=`date +"%T" `
@@ -8,19 +8,30 @@ commithash=`git show -s --format=%H`
 
 echo "Generating build $build ($builddate $buildtime)..."
 
+# For .NET Framework projects
 echo "// Auto-generated build information code file" > BuildInfo.cs
-echo "// Xlfdll Workstation" >> BuildInfo.cs
+echo "// (C) Xlfdll Workstation" >> BuildInfo.cs
 echo "" >> BuildInfo.cs
 echo "namespace Xlfdll" >> BuildInfo.cs
 echo "    {" >> BuildInfo.cs
 echo "    internal static class BuildInfo" >> BuildInfo.cs
 echo "        {" >> BuildInfo.cs
-#echo "        internal const string Version = \"$buildversion\";" >> BuildInfo.cs
+echo "        internal const string Version = \"$buildversion\";" >> BuildInfo.cs
 echo "        internal const string Build = \"$build\";" >> BuildInfo.cs
 echo "        internal const string BuildDate = \"$builddate\";" >> BuildInfo.cs
 echo "        internal const string BuildTime = \"$buildtime\";" >> BuildInfo.cs
 echo "        internal const string CommitHash = \"$commithash\";" >> BuildInfo.cs
 echo "        }" >> BuildInfo.cs
 echo "    }" >> BuildInfo.cs
+
+# For .NET Core projects
+# echo "<!-- Auto-generated build information code file -->" > Directory.Build.props
+# echo "<!-- (C) Xlfdll Workstation -->" >> Directory.Build.props
+# echo "" >> Directory.Build.props
+# echo "<Project>" >> Directory.Build.props
+# echo "  <PropertyGroup>" >> Directory.Build.props
+# echo "    <Version>$version.$build</Version>" >> Directory.Build.props
+# echo "  </PropertyGroup>" >> Directory.Build.props
+# echo "</Project>" >> Directory.Build.props
 
 echo "Done."
